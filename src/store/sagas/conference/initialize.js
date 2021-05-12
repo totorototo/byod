347a9f213e4fd0150b676f860c25694ed8c44fe2
Thomas Gilbert <thomas.gilbert@dolby.com>
2021-05-12 19:53:27 +0200

    fix refresh token issue

--- src/store/sagas/conference/initialize.js
+++ src/store/sagas/conference/initialize.js
@@ -1,11 +1,15 @@
 import { select, put, call } from "redux-saga/effects";
 import VoxeetSDK from "@voxeet/voxeet-web-sdk";
 
-import authenticate from "../authentication/authenticate";
+import { retrieveAccessToken } from "../authentication/authenticate";
 import { application } from "../../effects";
 
 export default function* initialize() {
   const token = yield select((state) => state.application.token);
-  yield call([VoxeetSDK, VoxeetSDK.initializeToken], token, authenticate);
+  yield call(
+    [VoxeetSDK, VoxeetSDK.initializeToken],
+    token,
+    retrieveAccessToken
+  );
   yield put(application.sdkInitialized());
 }
