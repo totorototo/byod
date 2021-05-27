af26fe3d9647627ed711d73f4a418858a840c332
Thomas Gilbert <thomas.gilbert@dolby.com>
2021-05-27 13:53:46 +0200

    add pwa support

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import configureStore from "./store/store";
 import { application } from "./store/actions";
+import * as serviceWorker from "./serviceWorker";
 
 const history = createBrowserHistory();
 
@@ -40,3 +41,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
